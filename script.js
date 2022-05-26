d9adebaadd7eeb0be97890b09e76257653526844
Cecilia Batista <ceciliadsbatista@gmail.com>
2022-05-26 15:21:47 -0300

    teste

---
+++ script.js
@@ -0,0 +1,81 @@
+
+
+
+const button = document.getElementById('convert-button')
+const select = document.getElementById('currency-select')
+
+
+
+
+const convertValue = async () => {
+    const inputReal = document.getElementById('input-real').value;
+    const realValueText = document.getElementById('real-value-text');
+    const currencyValueText = document.getElementById('currency-value-text');
+
+   const data = await fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL').then(response => response.json())
+
+
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const biticoin = data.BTCBRL.high
+    
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
+        { style: 'currency', currency: 'BRL' }
+    ).format(inputReal);
+
+    if (select.value === "US$ Dólar Americano") {
+        currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
+            { style: 'currency', currency: 'USD' }
+        ).format(inputReal / dolar);
+
+    }
+
+    if (select.value === "€ Euro") {
+        currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
+            { style: 'currency', currency: 'EUR' }
+        ).format(inputReal / euro);
+
+    }
+    if (select.value === "Bitcoin") {
+        currencyValueText.innerHTML = new Intl.NumberFormat('en-NZ',
+            { style: 'currency', currency: 'NZD' }
+        ).format(inputReal / biticoin);
+
+    }
+
+
+
+
+
+
+
+}
+
+
+changeCurrency = () => {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImg = document.getElementById("currency-img")
+    if (select.value === 'US$ Dólar Americano') {
+        currencyName.innerHTML = 'Dólar Americano  '
+        currencyImg.src = './img/estados-unidos (1) 1.svg'
+    }
+
+    if (select.value === '€ Euro') {
+        currencyName.innerHTML = 'Euro'
+        currencyImg.src = './img/euro.svg'
+    }
+    if (select.value === 'Bitcoin') {
+        currencyName.innerHTML = 'Bitcoin'
+        currencyImg.src = './img/Design sem nome (1) 1.svg'
+    }
+    convertValue()
+}
+
+
+button.addEventListener('click', convertValue)
+select.addEventListener('change', changeCurrency)
+
+
+
+
+
